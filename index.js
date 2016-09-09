2a57b2cbbcf8bb29193ed739e569c60f4ecacb25
Kenny Kropp <kenny@kropp.pro>
2016-09-09 18:20:07 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var math = require("mathjs");
+var tg = require("node-telegram-bot-api");
+
+var config = require("./config");
+
+if(config.telegram != null){
+
+
+
+
+
+}else{
+	console.error("Please enter telegram-bot credentials");
+}
+
+
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+*** telegram inline calc bot ***
